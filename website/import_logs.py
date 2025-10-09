7fad4bc70ca9e83d549f7eda1701a4cfb7734f73
alexanderbjorndal <112392346+alexanderbjorndal@users.noreply.github.com>
2025-10-09 10:57:27 +0000

    logs

--- website/import_logs.py
+++ website/import_logs.py
@@ -1,8 +1,8 @@
 # import_logs.py
 import re
 from datetime import datetime
-from website import create_app, db
-from website.models import Visitor
+from . import create_app, db
+from models import Visitor
 
 app = create_app()
 app.app_context().push()
